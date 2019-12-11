a51b2e40c161c49230aea1966bf532bcf79dc7cc
Timofey Alexeyvich Makhlay Kapitsa <tmakhlay2@gmail.com>
2019-12-10 21:02:31 -0800

    [Hard fix is fixed]

--- main.go
+++ main.go
@@ -39,9 +39,8 @@ func main() {
 		switch payload.(type) {
 
 		case github.PushPayload:
-			// release := payload.(github.PushPayload)
-			newMessage := "A commit has just been made to timomak/Golang-SlackAPI-GiphyAPI"
-			// newMessage := string(release.Pusher.Name) + " just made a commit to the " + string(release.Repository.FullName) + " repo.\nLook at the changes: " + string(release.Repository.HTMLURL) + "\n"
+			release := payload.(github.PushPayload)
+			newMessage := string(release.Pusher.Name) + " just made a commit to the " + string(release.Repository.FullName) + " repo.\nLook at the changes: " + string(release.Repository.HTMLURL) + "\n"
 
 			// fmt.Printf("%+v just made a commit to the %+v repo.\nLook at the changes: %+v\n", release.Pusher.Name, release.Repository.FullName, release.Repository.HTMLURL)
 			slackIt(newMessage, "portal-devs") // Message, Channel Name
@@ -59,5 +58,3 @@ func slackIt(message, channel string) {
 	// fmt.Println("SENDING MESSASSAGE TO SLACK CHANNEL:", message)
 	NotifySlackChannel(slackClient, message, channel)
 }
-
-// Tryna push
